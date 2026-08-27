abfcce4047b9cb24e9d8efb9c00e80b801cfaa11
Apna Naam <apna-github-email@example.com>
2026-08-28 01:35:17 +0530

    Add matrix transpose

---
+++ 2D Matrix - Visual Studio/matrix_transpose.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows, cols;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    cout << "Enter number of columns: ";
+    cin >> cols;
+
+    int matrix[100][100];
+
+    cout << "Enter matrix elements:\n";
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+
+    cout << "Transpose of Matrix:\n";
+    for (int j = 0; j < cols; j++)
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
