fb32831d53854a39da2d46b3aeb0429fcbfa5062
Apna Naam <apna-github-email@example.com>
2026-08-28 01:21:09 +0530

    Added Frequency of Element

---
+++ Searching - Visual Studio/frequency_of_element.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[100], n, key, count = 0;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "Enter element: ";
+    cin >> key;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key)
+            count++;
+    }
+
+    cout << "Frequency of " << key << " = " << count;
+
+    return 0;
+}
